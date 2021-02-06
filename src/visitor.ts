e5bb263424431dbbe352901263dfa2c199319807
Fabio Benedetti <fabio.benedetti@joyn.de>
2021-02-06 18:27:32 +0100

    fix: issue 95 unused type import

--- src/visitor.ts
+++ src/visitor.ts
@@ -102,9 +102,11 @@ export class ApolloNextSSRVisitor extends ClientSideBaseVisitor<
         `import { getApolloClient} from '${this.config.apolloClientInstanceImport}';`
       );
     }
-    this.imports.add(
-      `import type { NormalizedCacheObject } from '${this.config.apolloCacheImportFrom}';`
-    );
+    if (!this.config.apolloClientInstanceImport) {
+      this.imports.add(
+        `import type { NormalizedCacheObject } from '${this.config.apolloCacheImportFrom}';`
+      );
+    }
     if (this.config.customImports) {
       this.imports.add(this.config.customImports);
     }
